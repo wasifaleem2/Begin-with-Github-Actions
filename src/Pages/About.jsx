139982d6cb5ac7666617b485488b645efecd8155
wasif <wasif.aleem@braincrop.net>
2024-06-13 13:58:05 +0500

    feat:color change

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,5 @@
 function About() {
-  return <h1>About Page</h1>;
+  return <h1 style={{color: "blue"}}>About Page</h1>;
 }
 
 export default About;
--- src/Pages/Contact .jsx
+++ src/Pages/Contact .jsx
@@ -1,5 +1,5 @@
 function Contact() {
-  return <h1>Contact Page</h1>;
+  return <h1 style={{color: "skyblue"}}>Contact Page</h1>;
 }
 
 export default Contact;
