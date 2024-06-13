8ff0e1a311754952c58cece6afb7b69768da8d27
wasif <wasif.aleem@braincrop.net>
2024-06-13 15:52:44 +0500

    feat/add a 404 file for my project to navigate to index.html

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 function Home() {
-  return <h1 style={{color: "navy"}}>Home Page</h1>;
+  return (
+    <>
+      <h1 style={{ color: "navy" }}>Home Page</h1>
+      <p>Version 1.1</p>
+    </>
+  );
 }
 
 export default Home;
