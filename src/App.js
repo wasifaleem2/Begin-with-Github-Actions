24acc1c2806ac38fbd937cd97204edc7fda57098
wasif <wasif.aleem@braincrop.net>
2024-06-13 11:06:38 +0500

    add react routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+// App.js for React Router v6
+import "./App.css"
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from "./Pages/Contact ";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Through Github Action Updatess</h1>
+    <div className='app'>
+      <div className='app-nav'>
+        <NavBar />
+      </div>
+      <div className='app-page'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </div>
     </div>
   );
 }
