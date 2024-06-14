41fc6da3aa38812992e7fbc2a30cf42e3ef86673
wasif <wasif.aleem@braincrop.net>
2024-06-14 10:05:25 +0500

    adjustment for netlify deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter in
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/"> {/* Set basename here */}
+    <Router> {/* Set basename here */}
       <App />
     </Router>
   </React.StrictMode>,
